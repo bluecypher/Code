6c8338071ff9fe50858cf20842ed55e5545b39c3
bluecypher <40382144+bluecypher@users.noreply.github.com>
2020-09-28 05:17:53 +0000

    Add files via upload

---
+++ inorder.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    node  *left, *right;
+    node(int d)
+    {
+        data=d;
+        left=right=NULL;
+    }
+};
+unordered_map<node*,int> cnt;
+void inorder(node* t)
+{
+    stack<node* > s;
+    s.push(t);
+    while(!s.empty())
+    {
+        node* temp=s.top();
+        if(temp==NULL)
+        {
+            s.pop();
+            continue;
+        }
+        if(cnt[temp]==0){ s.push(temp->left);}
+        else if(cnt[temp]==1){ cout<<temp->data<<" ";}
+        else if(cnt[temp]==2){ s.push(temp->right);}
+        else s.pop();
+        cnt[temp]++;
+    }
+
+}
+
+
+int main()
+{
+    node* t=new node(1);
+    t->left=new node(2);
+    t->right=new node(3);
+    t->left->left=new node(4);
+    inorder(t);
+}
